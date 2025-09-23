11a42dd161f42a07baf908838557ee6673cee188
Vikram <vikramkini9@gmail.com>
2025-09-22 22:20:11 -0500

    change base url

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import { GlobalStyle, AppShell, PageWrap } from './styles/primitives.js'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import BackgroundFX from './components/BackgroundFX.jsx'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Projects from './pages/Projects.jsx'
-import Contact from './pages/Contact.jsx'
-import PumpJournalLanding from './pages/PumpJournalLanding.jsx'
-import PumpJournalSupport from './pages/PumpJournalSupport.jsx'
-import PumpJournalPrivacy from './pages/PumpJournalPrivacy.jsx'
-import Resume from './routes/Resume.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { GlobalStyle, AppShell, PageWrap } from './styles/primitives.js';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import BackgroundFX from './components/BackgroundFX.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Projects from './pages/Projects.jsx';
+import Contact from './pages/Contact.jsx';
+import PumpJournalLanding from './pages/PumpJournalLanding.jsx';
+import PumpJournalSupport from './pages/PumpJournalSupport.jsx';
+import PumpJournalPrivacy from './pages/PumpJournalPrivacy.jsx';
+import Resume from './routes/Resume.jsx';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/">
       <GlobalStyle />
       <AppShell>
         <BackgroundFX />
@@ -26,15 +26,15 @@ function App() {
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/projects/pumpjournal" element={<PumpJournalLanding />} />
-        <Route path="/pumpjournal/support" element={<PumpJournalSupport />} />
-        <Route path="/pumpjournal/privacy" element={<PumpJournalPrivacy />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
+            <Route path="/pumpjournal/support" element={<PumpJournalSupport />} />
+            <Route path="/pumpjournal/privacy" element={<PumpJournalPrivacy />} />
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
         </PageWrap>
         <Footer />
       </AppShell>
-    </HashRouter>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
